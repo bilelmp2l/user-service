61188f57632387983fb1424f3885d7f2ac43954e
Bilel Najjar <bilel.najjar@machinestalk.com>
2020-12-06 23:26:33 +0100

    add api get user by email

--- src/main/java/com/pfe/userservice/controller/UserController.java
+++ src/main/java/com/pfe/userservice/controller/UserController.java
@@ -3,11 +3,14 @@ package com.pfe.userservice.controller;
 import com.pfe.userservice.exceptions.NotExistUserException;
 import com.pfe.userservice.models.User;
 import com.pfe.userservice.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
+@Api("API method CRUD for User model")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
@@ -15,26 +18,42 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "add User on table user")
     @PostMapping(value = "/adduser")
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
+    @ApiOperation(value = "get all users")
     @GetMapping(value = "/all")
     public List<User> getUsers() {
         return userService.getUsers();
     }
 
+    @ApiOperation(value = "get user by id from database")
     @GetMapping(value = "/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @ApiOperation(value = "delete user by id")
     @DeleteMapping(value = "/delUser/{id}")
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
     }
 
+    @ApiOperation(value = "get user by email")
+    @GetMapping("/email/{email}")
+    public User getEmail(@PathVariable("email") String email) {
+        List<User> users =userService.getUsers();
+        for (User user : users) {
+            if (user.getEmail().equals(email))
+                 return user;
+        }
+        throw new NotExistUserException("user not exist");
+    }
+
+    @ApiOperation(value = "connect using login and password")
     @PostMapping("/login")
     public User loginUser(@RequestParam("email") String email, @RequestParam("password") String password) {
         User userLogged = null;
@@ -52,6 +71,7 @@ public class UserController {
 
     }
 
+    @ApiOperation(value = "update user by id")
     @PutMapping("/update/{id}")
     public User updateUser(@RequestBody User user, @PathVariable Long id) {
         User currentUser = userService.getUserById(id);
